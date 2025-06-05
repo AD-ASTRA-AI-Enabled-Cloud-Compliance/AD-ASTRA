31aec52e72d963b4a2e5705a3b90c88b69753bea
deepikaamehta <deepika@Deepikas-MacBook-Air.local>
2025-06-05 12:39:21 -0400

    refactor: from 2pdf PyMuPDF

--- backend/document_preprocess/src/__init__.py
+++ backend/document_preprocess/src/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from src.config.config import Config
 from dotenv import load_dotenv
 from src.routes import main_routes
-from .models.user_model import db
+# from .models.user_model import db
 
 load_dotenv()  # ✅ Load .env before app is created
 
@@ -13,7 +13,8 @@ CORS(app)
 config = Config().dev_config
 app.config.from_object(config)  # ✅ Apply config to Flask app
 
-db.init_app(app)
+# db.init_app(app)
 app.register_blueprint(main_routes)
 
-__all__ = ['app', 'db']
+# __all__ = ['app', 'db']
+__all__ = ['app']
