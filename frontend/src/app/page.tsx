c2ec3828362333d195f56067ebaabae38089329b
Puneet Sharma <112095510+Puneetsharmatech@users.noreply.github.com>
2025-06-06 13:25:09 -0400

    feat: added dashboard and login page, removed folder model/azure. (#13)
    * Clean start: Initial commit with no secrets
    
    * Configured to connect with Qdrant DB locally
    
    * adding file upload option in AI
    
    * Added MCP Functionality
    
    * File Structurized
    
    * --
    
    * Ignore pycache directory
    
    * Created Updated form page and added functionality of getting JSON
    
    * Update README.md
    
    * Directory Structured
    
    * Upload form field updation
    
    * -
    
    * Added Login and Dashboard page and removed directory model/azure
    
    ---------
    
    Co-authored-by: Puneet Sharma <Puneetsharma2@loyalistcollege.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
-  return (
-    <div>
+import React from "react";
+import UploadForm from "./management/uploadform"; // adjust path if needed
 
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <UploadForm />
     </div>
   );
 }
+
+export default App;
