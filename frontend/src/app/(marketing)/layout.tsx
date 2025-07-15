e141c959ebe61820247742a4f848202a180bbd86
cjmpmd <71334484+cjmpmd@users.noreply.github.com>
2025-07-15 08:25:31 -0400

    chore[UI]: landing page implementation

---
+++ frontend/src/app/(marketing)/layout.tsx
@@ -0,0 +1,6 @@
+
+import { MarketingLayout } from '@/components/landing/layout';
+
+export default function Layout(props: { children: React.ReactNode }) {
+  return <MarketingLayout>{props.children}</MarketingLayout>
+}
