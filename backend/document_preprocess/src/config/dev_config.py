fb108c78a36910e95d6704e0f9226c04ef5c3824
HarsimranKaur06 <harsimrankaur06@gmail.com>
2025-08-05 13:02:07 -0400

    Updating backend document_preprocess to use port 3020

--- backend/document_preprocess/src/config/dev_config.py
+++ backend/document_preprocess/src/config/dev_config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class DevConfig:
     ENV = os.getenv("FLASK_ENV", "development")
     DEBUG = os.getenv("FLASK_DEBUG", "True") == "True"
-    PORT = int(os.getenv("PORT", 3030))
+    PORT = int(os.getenv("PORT", 3020))
     HOST = os.getenv("HOST", "0.0.0.0")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
